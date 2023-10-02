f48a469732ea216ac137ba39050785ae0f383524
tomrcc <tom@cloudcannon.com>
2023-10-02 13:26:33 +1300

    Add pos relative to hero

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,5 +1,5 @@
 export default ({ title, button, image, image_alt }) => {
-  return <div className="flex flex-col justify-items-center p-5 md:p-10 lg:p-20">
+  return <div className="relative flex flex-col justify-items-center p-5 md:p-10 lg:p-20">
     <h1 className="relative z-10 text-5xl lg:text-6xl font-bold py-3 md:py-5 text-white">{ title }</h1>
     {button && <a href={button.button_path} className="relative z-10 bg-gray-100 w-fit py-3 px-8 rounded-lg">{ button.button_text }</a>}
     <img
