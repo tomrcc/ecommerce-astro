86458db87f8f9473c9bf7faf5456267f666abf7f
tomrcc <tom@cloudcannon.com>
2023-10-03 13:35:59 +1300

    Try to fix launch cart

--- src/components/shop-items/shop-items.jsx
+++ src/components/shop-items/shop-items.jsx
@@ -4,7 +4,7 @@ export default function ShopItems({ shop_items, page_size }) {
   if (shop_items.length > 0) {
     return (
       <section className="grid grid-cols-3 gap-8 py-5 px-5 md:px-10 lg:px-20">
-        {shop_items.slice(0, page_size).map((shopItem, i) => {
+        {shop_items.slice(0, 1).map((shopItem, i) => {
           return (
             <div className="py-5" key={i}>
               <img
@@ -13,9 +13,6 @@ export default function ShopItems({ shop_items, page_size }) {
                 alt={shopItem.image.alt}
               />
               <h1 className="py-2">{shopItem.name}</h1>
-              <launch-cart-additem price-id="price_1Nww7HJS2vAZDHNripQNJjh6">
-                <button>Add to cart!</button>
-              </launch-cart-additem>
             </div>
           );
         })}
